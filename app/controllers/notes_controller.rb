894d8502fbe95765b233c35b5f216d01c3d8a5b8
Mike Boyd <mike.boyd@radicalbear.com>
2020-07-21 21:52:59 -0400

    Add new and create actions on Notes controller with associated view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,20 @@
 class NotesController < ApplicationController
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render :new
+    end
+  end
+  private
+
+    def note_params
+      params.require(:note).permit(:title, :content)
+    end
 end
