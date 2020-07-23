213d986086cd7d27330fc1a9888dc7df661be625
Mike Boyd <mike.boyd@radicalbear.com>
2020-07-23 16:20:05 -0400

    Update initializer for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,11 +112,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password:ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"]
   }
 
   Rails.application.routes.default_url_options[:host] = ENV['URL_HOST']
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'mike@pterabytes.com'
+  config.mailer_sender = 'nucelusnotesapp@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
