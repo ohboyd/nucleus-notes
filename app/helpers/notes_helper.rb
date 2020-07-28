7f95893c933aa09dc7f43a8c43e44deee8dad63e
Mike Boyd <mike.boyd@radicalbear.com>
2020-07-28 07:23:34 -0400

    Create a small method to randomly select next task worth being worked on

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -2,4 +2,8 @@ module NotesHelper
   def show_random_button(notes)
     notes.select(&:task).size >= 5
   end
+
+  def random_task(notes)
+    notes.where(task: true).order(:created_at).limit(5).sample
+  end
 end
