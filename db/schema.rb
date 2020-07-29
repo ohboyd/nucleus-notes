0771eeeb87486293719875a8c2bf5d2ab0a21935
Mike Boyd <mike.boyd@radicalbear.com>
2020-07-29 16:34:19 -0400

    Add new 'complete' column to the notes table so notes can be finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_070214) do
+ActiveRecord::Schema.define(version: 2020_07_29_195730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_070214) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.boolean "task", default: false, null: false
+    t.boolean "complete", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
