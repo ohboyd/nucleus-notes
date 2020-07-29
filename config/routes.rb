13f930eb2231374276c5f3ab132068815ebed977
Mike Boyd <mike.boyd@radicalbear.com>
2020-07-29 03:25:12 -0400

    Update routes for better UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => {
+    :verify_authy => "/verify-token",
+    :enable_authy => "/enable-two-factor",
+    :verify_authy_installation => "/verify-installation",
+    :authy_onetouch_status => "/onetouch-status"
+  }
 
   get 'welcome/index'
 
