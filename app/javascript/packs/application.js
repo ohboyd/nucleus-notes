9991de7ad7e1f15c616c8f15cba7d82c6cd455ac
Mike Boyd <mike.boyd@radicalbear.com>
2020-07-30 23:44:37 -0400

    Import jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,9 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 
+import $ from 'jquery';
+global.$ = jQuery;
+
 import 'bootstrap'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
