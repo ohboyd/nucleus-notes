414444848303a330f0cf51b127a4611908498903
Mike Boyd <mike.boyd@radicalbear.com>
2020-07-31 07:44:16 -0400

    Integrate with giphy API to GET unique, random gifs

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -6,4 +6,12 @@ module NotesHelper
   def random_task(notes)
     notes.unfinished.where(task: true).order(:created_at).limit(5).sample
   end
+
+  def grab_giphy(celebrations)
+    url="http://api.giphy.com/v1/gifs/search?q=#{celebrations}&api_key=#{ENV["GIPHY_API_KEY"]}&limit=1"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    buffer = resp.body
+    result = JSON.parse(buffer)
+    source = result.fetch('data')[0]['embed_url']
+  end
 end
